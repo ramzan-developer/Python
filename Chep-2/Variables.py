82951b7e9eb736488868ef5826980a1f05c02ed7
= <ramzan.webdeveloper@gmail.com>
2025-08-05 21:38:27 +0530

    solved problems of variables and operators in python!

---
+++ Chep-2/Variables.py
@@ -0,0 +1,49 @@
+
+# 1)Write a python program to add two numbers.
+
+# a = 25
+# b = 30
+
+# print(b + a)
+
+# 2) Write a python program to find remainder when a number is divided by z.
+# a = 25
+# b = 5
+
+# c = a % b 
+# print("The remainder is: ", c)  
+
+
+# Check the type of variable assigned using input () function.
+
+# name = input("Enter your name: ")
+
+# b = type(name)
+# print("The type of the variable is: ", b)
+
+
+# Use comparison operator to find out whether ‘a’ given variable a is greater than 
+# ‘b’ or not. Take a = 34 and b = 80 
+# a = 36
+# b = 80
+
+# c = a > b
+# print("Is a greater than b? ", c)
+
+
+
+
+# Write a python program to find an average of two numbers entered by the user.
+# a = int(input("Enter a value for a: "))
+# b = int(input("Enter a value for b: "))
+
+# c = (a + b)/2
+# print("The average of a and b is: ", c)
+
+
+
+# Write a python program to calculate the square of a number entered by the user.
+# a = int(input("Enter a value for a: "))
+
+# b = a ** 2
+# print("The square of a is: ", b)
