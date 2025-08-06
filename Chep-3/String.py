eeb25fda56ff65938e36a672d19a0ee540a4765e
= <ramzan.webdeveloper@gmail.com>
2025-08-06 21:32:06 +0530

    Learning Strings and applying some methods on it.

---
+++ Chep-3/String.py
@@ -0,0 +1,33 @@
+# 1) Write a python program to display a user entered name followed by Good Afternoon using input () function. 
+
+# name = input("Enter your name" )
+# print("Good Afternoon! ",name)
+# print(f"Good Afternoon! {name}")
+
+
+# 2) Write a program to fill in a letter template given below with name and date.
+# name = input("Enter your name: ")
+# date = input("Enter the date: ")
+
+# letter = '''  
+# Dear {}, 
+# You are selected! 
+# {} 
+# '''
+# print(letter.format(name, date))
+
+
+# 3.Write a program to detect double space in a string. 
+# string = "Hello,  World!"
+# print(string.find("  "))
+# print(string)
+
+# 4) Write a program to replace double space with single space in a string.
+# print(string.replace("  ", " "))         
+
+#Strings are immutable which means they cannot be changed. it will return a new string with the changes.
+
+
+# letter = "Dear Harry,\nthis python course is nice.\nhanks!"
+# print(letter)
+
